3fb8b1f5ffa4a892e69d1751997e92b7bac69a9b
Rtanakar <ratnakarmishra@623gmail.com>
2026-01-14 18:17:20 +0530

    More Reels project functionality

--- reels project/script.js
+++ reels project/script.js
@@ -1,17 +1,19 @@
 const reels = [
   {
+    ismuted: false,
     username: "codewithayush",
     likeCount: 14820,
     isLiked: false,
     commentCount: 423,
     shareCount: 92,
-    isFollowed: true,
+    isFollowed: false,
     caption: "Dark mode > light mode. Change my mind.",
-    video: "./reels/video1.mp4",
+    video: "./reels/video5.mp4",
     userprofile:
       "https://images.unsplash.com/photo-1613915617430-8ab0fd7c6baf?q=80&w=930&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
+    ismuted: true,
     username: "designbysan",
     likeCount: 9820,
     isLiked: true,
@@ -19,11 +21,12 @@ const reels = [
     shareCount: 41,
     isFollowed: false,
     caption: "UI tip: Padding is personality. Give your elements some space.",
-    video: "./reels/video2.mp4",
+    video: "./reels/video6.mp4",
 
     userprofile: "https://images.unsplash.com/photo-1531427186611-ecfd6d936c79",
   },
   {
+    ismuted: true,
     username: "frontend.ninja",
     likeCount: 22150,
     isLiked: false,
@@ -31,11 +34,12 @@ const reels = [
     shareCount: 138,
     isFollowed: true,
     caption: "When flexbox finally aligns the way you wanted 😭🔥",
-    video: "./reels/video3.mp4",
+    video: "./reels/video7.mp4",
 
     userprofile: "https://images.unsplash.com/photo-1552374196-c4e7ffc6e126",
   },
   {
+    ismuted: true,
     username: "travelwithriya",
     likeCount: 54200,
     isLiked: false,
@@ -48,6 +52,7 @@ const reels = [
     userprofile: "https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e",
   },
   {
+    ismuted: true,
     username: "daily.dev.quotes",
     likeCount: 3120,
     isLiked: true,
@@ -60,6 +65,7 @@ const reels = [
     userprofile: "https://images.unsplash.com/photo-1599566150163-29194dcaad36",
   },
   {
+    ismuted: true,
     username: "fitnessbymegha",
     likeCount: 27450,
     isLiked: false,
@@ -72,6 +78,7 @@ const reels = [
     userprofile: "https://images.unsplash.com/photo-1595152772835-219674b2a8a6",
   },
   {
+    ismuted: true,
     username: "streetfoodlover",
     likeCount: 68000,
     isLiked: true,
@@ -84,6 +91,7 @@ const reels = [
     userprofile: "https://images.unsplash.com/photo-1552058544-f2b08422138a",
   },
   {
+    ismuted: true,
     username: "musicbytara",
     likeCount: 14500,
     isLiked: false,
@@ -96,6 +104,7 @@ const reels = [
     userprofile: "https://images.unsplash.com/photo-1502685104226-ee32379fefbe",
   },
   {
+    ismuted: true,
     username: "techreviews101",
     likeCount: 23180,
     isLiked: true,
@@ -108,6 +117,7 @@ const reels = [
     userprofile: "https://images.unsplash.com/photo-1511367461989-f85a21fda167",
   },
   {
+    ismuted: true,
     username: "learnanimations",
     likeCount: 18740,
     isLiked: false,
@@ -122,49 +132,60 @@ const reels = [
 ];
 
 const allReels = document.querySelector(".all-reels");
-let data = "";
-reels.forEach((elem) => {
-  data += `
-          <div class="reel">
-          <video autoplay muted loop src=${elem.video}></video>
 
+function reelsPlay() {
+  let data = "";
+
+  reels.forEach((elem, idx) => {
+    data += `
+          <div class="reel">
+          <video autoplay loop ${elem.ismuted ? "muted" : ""} src=${
+      elem.video
+    }></video>
+        <div class="mute" id=${idx}>
+          ${
+            elem.ismuted
+              ? '<i class="ri-volume-mute-fill"></i>'
+              : '<i class="ri-volume-up-line"></i>'
+          }
+    </div>
           <div class="bottom">
             <div class="user">
               <img
                 src=${elem.userprofile}
                 alt="">
               <h4>${elem.username}</h4>
-              <button>${elem.isFollowed ? "unfollow" : "Follow"}</button>
+              <button id=${idx} class="follow">${
+      elem.isFollowed ? "unfollow" : "Follow"
+    }</button>
             </div>
             <h3>${elem.caption}
             </h3>
           </div>
 
           <div class="right">
-            <div class="like">
-              <h4 class="like-icon">
-              ${
+            <div id=${idx} class="like">
+              <h4 class="like-icon icon">${
                 elem.isLiked
-                  ? '<i class="ri-heart-3-fill love"></i>'
+                  ? '<i class="love ri-heart-3-fill"></i>'
                   : '<i class="ri-heart-3-line"></i>'
-              }
-                <h6>${elem.likeCount}</h6>
-              </h4>
+              }</h4>
+              <h6>${elem.likeCount}</h6>
             </div>
             <div class="comment">
-              <h4 class="comment-icon">
+              <h4 class="comment-icon icon">
                 <i class="ri-chat-1-line"></i>
                 <h6>${elem.commentCount}</h6>
               </h4>
             </div>
             <div class="share">
-              <h4 class="share-icon">
+              <h4 class="share-icon icon">
                 <i class="ri-share-forward-line"></i>
                 <h6>${elem.shareCount}</h6>
               </h4>
             </div>
             <div class="menu">
-              <h4 class="menu-icon">
+              <h4 class="menu-icon icon">
                 <i class="ri-more-2-fill"></i>
               </h4>
             </div>
@@ -172,7 +193,39 @@ reels.forEach((elem) => {
         </div>
   `;
 
-  console.log(elem);
-});
+    // console.log(elem);
+  });
 
-allReels.innerHTML = data;
+  allReels.innerHTML = data;
+}
+
+reelsPlay();
+
+allReels.addEventListener("click", (dets) => {
+  if (dets.target.className === "like") {
+    if (!reels[dets.target.id].isLiked) {
+      reels[dets.target.id].isLiked = true;
+      reels[dets.target.id].likeCount++;
+    } else {
+      reels[dets.target.id].isLiked = false;
+      reels[dets.target.id].likeCount--;
+    }
+  }
+
+  if (dets.target.className === "follow") {
+    if (!reels[dets.target.id].isFollowed) {
+      reels[dets.target.id].isFollowed = true;
+    } else {
+      reels[dets.target.id].isFollowed = false;
+    }
+  }
+
+  if (dets.target.className === "mute") {
+    if (!reels[dets.target.id].ismuted) {
+      reels[dets.target.id].ismuted = true;
+    } else {
+      reels[dets.target.id].ismuted = false;
+    }
+  }
+  reelsPlay();
+});
